9cd2b6636fe769ae525a4f496a47a0a9f7355817
Krystian Bilski <krystian.bilski@bwigroup.com>
2021-08-23 09:38:19 +0200

    103

--- nowa-aplikacja/src/app/app.component.ts
+++ nowa-aplikacja/src/app/app.component.ts
@@ -8,4 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Angular';
   imie = 'Krystian';
-}
+  wyswietl = true;
+  osoby = [
+    { imie: 'Krystian', wiek: 25 },
+    { imie: 'Damian', wiek: 45 },
+    { imie: 'Marcin', wiek: 33 },
+    { imie: 'Karol', wiek: 41 },
+    { imie: 'Grzegorz', wiek: 56 },
+    { imie: 'Kacper', wiek: 21 },
+  ];
+
+
+  wyslij = () => {
+    this.wyswietl = !this.wyswietl
+  };
+};
