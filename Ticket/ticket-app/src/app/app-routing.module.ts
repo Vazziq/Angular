8e7015ca01363a069f3e339c2bffb8034a6a36c6
Krystian Bilski <krystian.bilski@bwigroup.com>
2021-09-10 08:18:01 +0200

    1

--- Ticket/ticket-app/src/app/app-routing.module.ts
+++ Ticket/ticket-app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BadgeComponent } from './badge/badge.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: 'login' },
+  { path: 'badge', component: BadgeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
